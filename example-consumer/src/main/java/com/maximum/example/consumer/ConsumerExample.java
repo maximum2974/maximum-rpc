df782cfbd81b6b4e331d0cf5d42999d937a0e637
maximum <1291185069@qq.com>
2025-01-02 02:34:28 +0800

    feat: 接口Mock

---
+++ example-consumer/src/main/java/com/maximum/example/consumer/ConsumerExample.java
@@ -0,0 +1,21 @@
+package com.maximum.example.consumer;
+
+import com.maximum.example.common.model.User;
+import com.maximum.example.common.service.UserService;
+import com.maximum.maximumrpc.proxy.ServiceProxyFactory;
+
+public class ConsumerExample {
+    public static void main(String[] args) {
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
+        User user = new User();
+        user.setName("maximum");
+        User newUser = userService.getUser(user);
+        if(newUser != null){
+            System.out.println(newUser.getName());
+        }else{
+            System.out.println("user == null");
+        }
+        long number = userService.getNumber();
+        System.out.println(number);
+    }
+}
